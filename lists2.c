4c920139e25fa43d5fc850a1bce954250e7fa141
Sarahkiss215 <skisiangani5@gmail.com>
2023-08-17 23:45:56 -0700

    Add list file with functions of list_var linked list

---
+++ lists2.c
@@ -0,0 +1,60 @@
+#include "shell.h"
+
+/**
+ * *add_var_end - adds a variable at the end of list_var.
+ * @head: head of the linked list.
+ * @len_var: length of the variable.
+ * @val: value of the variable.
+ * @len_val: length of the value.
+ *
+ * Return: address of the head.
+ */
+list_var *add_var_end(list_var **head, int len_var, char *val, int len_val)
+{
+	list_var *newvar, *temp;
+
+	newvar = malloc(sizeof(list_var));
+	if (newvar == NULL)
+	{
+		return (NULL);
+	}
+	newvar->len_var = len_var;
+	newvar->val = val;
+	newvar->len_val = len_val;
+	newvar->next = NULL;
+	temp = *head;
+	if (temp == NULL)
+	{
+		*head = newvar;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = newvar;
+	}
+	return (*head);
+}
+
+/**
+ * free_list_var - frees list_var list
+ * @head: head of the linked list.
+ *
+ * Return: Nothing.
+ */
+void free_list_var(list_var **head)
+{
+	list_var *temp;
+	list_var *newnode;
+
+	if (head != NULL)
+	{
+		newnode = *head;
+		while ((temp = newnode) != NULL)
+		{
+			newnode = newnode->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
