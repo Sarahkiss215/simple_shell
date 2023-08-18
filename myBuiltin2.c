afd119650c1537eaf1d7d8e333f645850ec37ecd
Sarahkiss215 <skisiangani5@gmail.com>
2023-08-18 00:11:39 -0700

    Add file with help information on builtin cd and alias

---
+++ myBuiltin2.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+
+/**
+ * info_helpbuiltin - Help information for the builtin help.
+ *
+ * Return: Nothing
+ */
+void info_helpbuiltin(void)
+{
+	char *help = "help: help [-dms] [pattern ...]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tDisplay information about builtin commands.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "Displays brief summaries of builtin commands.\n";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
+
+/**
+ * info_cdbuiltin - Help information for the builtin cd command.
+ *
+ * Return: Nothing.
+ */
+void info_cdbuiltin(void)
+{
+	char *help = "cd: cd [-L|[-P [-e]] [-@]] [dir]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tChange the shell working directory.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
+
+/**
+ * info_alias - Help information for the builtin alias.
+ *
+ * Return: Nothing.
+ */
+void info_alias(void)
+{
+	char *help = "alias: alias [-p] [name[=value]...]\n";
+
+	write(STDOUT_FILENO, help, _strlen(help));
+	help = "\tDefine or display aliases.\n ";
+	write(STDOUT_FILENO, help, _strlen(help));
+}
